0b92960fce54cb4fcffc1dd3fc245cbb8c07bc6a
Boyquotes <webmaster@linux-live-cd.org>
2016-03-13 04:38:30 +0100

    Redirection vers parcours

--- patrick/js/crud.js
+++ patrick/js/crud.js
@@ -6,9 +6,12 @@ $('.submit_login').click(function(e){
      url : 'http://iness.simplon.co/crud.php',
      type : 'POST', // Le type de la requête HTTP, ici devenu POST
      data : 'pseudo_session=' + pseudo_session , // On fait passer nos variables, exactement comme en GET, au script more_com.php
-     dataType : 'html'
+     dataType : 'html',
+     success : function(code_html, statut){ // code_html contient le HTML renvoyé
+       console.log('ici');
+       window.location.replace("parcours.html");
+     }
   });
-  window.location.replace("parcours.html");
 });
 
 /*function charger(){
